a18c8b166beaf6145a4fb7857ae4cec67b42d86f
Ben Brown <benbrown@gmail.com>
2023-01-04 23:30:45 -0600

    experimental feed view

--- routes/admin.js
+++ routes/admin.js
@@ -21,7 +21,10 @@ import {
     isFollowing,
     getInboxIndex,
     getInbox,
-    writeInboxIndex
+    writeInboxIndex,
+    isReplyToMyPost,    
+    isReplyToFollowing
+
 } from '../lib/account.js';
 import {
     fetchUser
@@ -253,6 +256,118 @@ router.get('/notifications', async (req, res) => {
     });
 });
 
+
+router.get('/feeds/:handle?', async (req, res) => {
+
+    const following = getFollowing();
+    const likes = await getLikes();
+    const boosts = await getBoosts();
+
+
+    const feeds = following.map((follower) => {
+        // posts in index by this author
+        // this is probably expensive.
+        // what we really need to do is look from this person by date
+        // and if we sort right it should be reasonable?
+        // and we just return unread counts for everything?
+        const posts = INDEX.filter((p) => p.actor == follower.actorId);
+        
+        // find most recent post
+        const mostRecent = posts.sort((a,b) => {
+            if (a.published > b.published) {
+                return -1;
+            } else if (a.published < b.published) {
+                return 1;
+            } else {
+                return 0;
+            }
+        })[0]?.published || null;
+
+        return {
+            actorId: follower.actorId,
+            postCount: posts.length,
+            mostRecent: mostRecent,
+        }
+    })
+
+    feeds.sort((a,b) => {
+        if (a.mostRecent > b.mostRecent) {
+            return -1;
+        } else if (a.mostRecent < b.mostRecent) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
+    let activitystream;
+
+    if (req.params.handle) {
+        const account = await fetchUser(req.params.handle);
+        activitystream = await Promise.all(INDEX.filter((p) => p.actor == account.actor.id).sort((a,b) => {
+            if (a.published > b.published) {
+                return -1;
+            } else if (a.published < b.published) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }).map(async (p) => {
+            try {
+                let boost, booster;
+                const mainacct = await fetchUser(p.actor);
+                let actor = mainacct.actor;
+                let post = await getActivity(p.id);
+                if (post.type === 'Announce') {
+                    boost = post;
+                    booster = actor;
+                    try {
+                        post = await getActivity(boost.object);
+                        const acct = await fetchUser(post.attributedTo);
+                        actor = acct.actor;
+                    } catch (err) {
+                        console.error(err);
+                        console.error('Could not fetch boosted post...', boost.object);
+                        // return;
+                    }
+                }
+        
+                if (actor) {
+                    actor.isFollowing = isFollowing(actor.id);
+        
+                    // determine if this post has already been liked
+                    post.isLiked = (likes.some((l) => l.activityId === post.id)) ? true : false;
+                    post.isBoosted = (boosts.some((l) => l.activityId === post.id)) ? true : false;
+        
+                } else {
+                    console.error('Post without an actor found', n.note.id);
+                }
+
+                return({
+                    boost,
+                    booster,
+                    note: post,
+                    actor: actor
+                });
+            } catch (err) {
+                console.error('error while loading post from index',err);
+            }
+        }));
+    }
+
+    // res.json(activitystream);
+    // return;
+    res.render('feeds', {
+        layout: 'private',
+        me: ActivityPub.actor,
+        feeds,
+        activitystream
+        // inboxes,
+        // inbox,
+        // error
+    });
+
+});
 
 router.get('/dms/:handle?', async (req, res) => {
     const inboxIndex = getInboxIndex();
