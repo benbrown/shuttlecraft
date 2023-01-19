8c8b6ccedb4093902b8061f000ab434b4a110e01
Daniel Sieradski <daniel@self.agency>
2023-01-18 20:37:03 -0500

    no unused vars
    Signed-off-by: Daniel Sieradski <daniel@self.agency>

--- public/app.js
+++ public/app.js
@@ -25,16 +25,16 @@ const setCookie = (name,value,days) => {
     }
     document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
-const getCookie = (name) => {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for(let i=0;i < ca.length;i++) {
-        let c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
+// const getCookie = (name) => {
+//     const nameEQ = name + "=";
+//     const ca = document.cookie.split(';');
+//     for(let i=0;i < ca.length;i++) {
+//         let c = ca[i];
+//         while (c.charAt(0)==' ') c = c.substring(1,c.length);
+//         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+//     }
+//     return null;
+// }
 
 const app = {
     newPosts: 0,
