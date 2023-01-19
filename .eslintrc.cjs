7e52602dd1d515fa0905c142af90f92d56077f77
Daniel Sieradski <daniel@self.agency>
2023-01-18 19:54:11 -0500

    add eslint, prettier, husky, lint-staged
    Signed-off-by: Daniel Sieradski <daniel@self.agency>

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['standard', 'prettier'],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
