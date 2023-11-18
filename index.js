6b756b844e47bdade011bb71523d725302fc367e
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2023-11-18 00:46:42 -0800

    add jsdocs

--- index.js
+++ index.js
@@ -1,52 +1,181 @@
-import express from 'express';
-import { create } from 'express-handlebars';
+// index.js is the start point for our project
+
+// middleware to parse request bodies in different formats (e.g., JSON)
+import bodyParser from 'body-parser';
+
+// middleware to parse cookies in incoming requests
 import cookieParser from 'cookie-parser';
 
+// Cross-Origin Resource Sharing (CORS) middleware for enabling cross-origin requests
+import cors from 'cors';
+
+// the dotenv module for loading environment variables from a .env file
 import dotenv from 'dotenv';
 
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import http from 'http';
+// the Express.js framework for building web applications
+import express from 'express';
+
+// middleware for implementing basic authentication in Express
 import basicAuth from 'express-basic-auth';
+
+// the Handlebars view engine for rendering dynamic HTML content
+import { create } from 'express-handlebars';
+
+// the built-in Node.js HTTP module for creating an HTTP server
+import http from 'http';
+
+// the Moment.js library for handling dates and times
 import moment from 'moment';
+
 import { ActivityPub } from './lib/ActivityPub.js';
 import { ensureAccount } from './lib/account.js';
-import { account, webfinger, inbox, outbox, admin, notes, publicFacing } from './routes/index.js';
+
+import { 
+  account, 
+  webfinger, 
+  inbox, 
+  outbox, 
+  admin, 
+  notes, 
+  publicFacing 
+} from './routes/index.js';
+
 
 // load process.env from .env file
 dotenv.config();
 const { USERNAME, PASS, DOMAIN, PORT } = process.env;
-['USERNAME', 'PASS', 'DOMAIN'].forEach(required => {
-  if (!process.env[required]) {
-    console.error(`Missing required environment variable: \`${required}\`. Exiting.`);
-    process.exit(1);
-  }
-});
 
+const env_variables = ['USERNAME', 'PASS', 'DOMAIN'];
 const PATH_TO_TEMPLATES = './design';
+
+/**
+ * Check the existence of required environment variables.
+ *
+ * @param {string[]} env_variables - An array of environment variable names that are required.
+ * @throws {Error} Throws an error and exits the process if any required environment variable is missing.
+ */
+function checkRequiredEnvironmentVariables(env_variables) {
+  env_variables.forEach((reqd_variable) => {
+    /**
+     * Check if the required environment variable is missing.
+     * If missing, log an error message and exit the process.
+     *
+     * @example
+     * // Example usage:
+     * checkRequiredEnvironmentVariables(['PORT', 'DATABASE_URL']);
+     */
+    if (!process.env[reqd_variable]) {
+      console.error(`Missing required environment variable: \`${reqd_variable}\`. Exiting.`);
+      process.exit(1);
+    }
+  });
+}
+checkRequiredEnvironmentVariables(env_variables);
+
 const app = express();
+/**
+ * Handlebars helper functions for custom template rendering.
+ *
+ * @typedef {Object} HandlebarsHelpers
+ * @property {Function} isVideo - Check if a string contains 'video' and execute the provided block if true.
+ * @property {Function} isImage - Check if a string contains 'image' and execute the provided block if true.
+ * @property {Function} isEq - Check if two values are equal and execute the provided block if true.
+ * @property {Function} or - Logical OR between two values.
+ * @property {Function} timesince - Format a date to show the time elapsed since the specified date.
+ * @property {Function} getUsername - Get the username using the ActivityPub module.
+ * @property {Function} stripProtocol - Remove 'https://' from the beginning of a string.
+ * @property {Function} stripHTML - Remove HTML tags from a string.
+ */
+
+/**
+ * Create an instance of Handlebars with custom helpers.
+ *
+ * @type {Handlebars}
+ * @see {@link https://handlebarsjs.com/api-reference/helpers.html}
+ */
 const hbs = create({
   helpers: {
+    /**
+     * Check if a string contains 'video' and execute the provided block if true.
+     * @function
+     * @param {string} str - The string to check.
+     * @param {Object} options - Handlebars options object.
+     * @returns {string} - The rendered block if the condition is true.
+     */
     isVideo: (str, options) => {
       if (str && str.includes('video')) return options.fn(this);
     },
+
+    /**
+     * Check if a string contains 'image' and execute the provided block if true.
+     * @function
+     * @param {string} str - The string to check.
+     * @param {Object} options - Handlebars options object.
+     * @returns {string} - The rendered block if the condition is true.
+     */
     isImage: (str, options) => {
       if (str && str.includes('image')) return options.fn(this);
     },
+
+    /**
+     * Check if two values are equal and execute the provided block if true.
+     * @function
+     * @param {*} a - The first value.
+     * @param {*} b - The second value.
+     * @param {Object} options - Handlebars options object.
+     * @returns {string} - The rendered block if the condition is true.
+     */
     isEq: (a, b, options) => {
       // eslint-disable-next-line
       if (a == b) return options.fn(this);
     },
+
+    /**
+     * Logical OR between two values.
+     * @function
+     * @param {*} a - The first value.
+     * @param {*} b - The second value.
+     * @param {Object} options - Handlebars options object.
+     * @returns {*} - The result of the logical OR operation.
+     */
     or: (a, b, options) => {
       return a || b;
     },
+
+    /**
+     * Format a date to show the time elapsed since the specified date.
+     * @function
+     * @param {Date} date - The date to be formatted.
+     * @returns {string} - The formatted time elapsed string.
+     */
     timesince: date => {
       return moment(date).fromNow();
     },
+
+    /**
+     * Get the username using the ActivityPub module.
+     * @function
+     * @param {*} user - The user object.
+     * @returns {string} - The username.
+     */
     getUsername: user => {
       return ActivityPub.getUsername(user);
     },
+
+    /**
+     * Remove 'https://' from the beginning of a string.
+     * @function
+     * @param {string} str - The string to process.
+     * @returns {string} - The string with 'https://' removed.
+     */
     stripProtocol: str => str.replace(/^https:\/\//, ''),
+
+    /**
+     * Remove HTML tags from a string.
+     * @function
+     * @param {string} str - The string containing HTML tags.
+     * @returns {string} - The string with HTML tags removed.
+     */
     stripHTML: str =>
       str
         .replace(/<\/p>/, '\n')
@@ -55,56 +184,80 @@ const hbs = create({
   }
 });
 
-app.set('domain', DOMAIN);
-app.set('port', process.env.PORT || PORT || 3000);
-app.set('port-https', process.env.PORT_HTTPS || 8443);
-app.engine('handlebars', hbs.engine);
-app.set('views', PATH_TO_TEMPLATES);
-app.set('view engine', 'handlebars');
-app.use(
-  bodyParser.json({
-    type: 'application/activity+json'
-  })
-); // support json encoded bodies
-app.use(
-  bodyParser.json({
-    type: 'application/json'
-  })
-); // support json encoded bodies
-app.use(
-  bodyParser.json({
-    type: 'application/ld+json'
-  })
-); // support json encoded bodies
+const setExpressApp = (app) => {
+  app.set('domain', DOMAIN);
+  app.set('port', process.env.PORT || PORT || 3000);
+  app.set('port-https', process.env.PORT_HTTPS || 8443);
+  app.engine('handlebars', hbs.engine);
+  app.set('views', PATH_TO_TEMPLATES);
+  app.set('view engine', 'handlebars');
+  app.use(
+    bodyParser.json({
+      type: 'application/activity+json'
+    })
+  ); // support json encoded bodies
+  app.use(
+    bodyParser.json({
+      type: 'application/json'
+    })
+  ); // support json encoded bodies
+  app.use(
+    bodyParser.json({
+      type: 'application/ld+json'
+    })
+  ); // support json encoded bodies
 
-app.use(cookieParser());
+  app.use(cookieParser());
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-); // support encoded bodies
+  app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+  ); // support encoded bodies
+}
 
-// basic http authorizer
-const basicUserAuth = basicAuth({
-  authorizer: asyncAuthorizer,
-  authorizeAsync: true,
-  challenge: true
-});
+setExpressApp(app);
 
-function asyncAuthorizer(username, password, cb) {
+
+/**
+ * Asynchronous basic authorization function for Express.js.
+ *
+ * @param {string} username - The provided username for authorization.
+ * @param {string} password - The provided password for authorization.
+ * @param {Function} callback - The callback function to be called upon authorization completion.
+ * @param {Error} callback.error - An error object if an error occurred during authorization, or null if successful.
+ * @param {boolean} callback.authorized - A boolean indicating whether the user is authorized.
+ *
+ * @example
+ * // Example usage:
+ * asyncAuthorizer('admin', 'password123', (error, authorized) => {
+ *   if (error) {
+ *     console.error(error.message);
+ *   } else {
+ *     console.log(`User is authorized: ${authorized}`);
+ *   }
+ * });
+ */
+const asyncAuthorizer = (username, password, callback) => {
   let isAuthorized = false;
+  // Check if the provided password matches the hardcoded username
   const isPasswordAuthorized = username === USERNAME;
+
+  // Check if the provided username matches the hardcoded password
   const isUsernameAuthorized = password === PASS;
+
+  // Set isAuthorized to true if both username and password are authorized
   isAuthorized = isPasswordAuthorized && isUsernameAuthorized;
+
+  // Invoke the callback with the authorization result
   if (isAuthorized) {
-    return cb(null, true);
+    return callback(null, true);
   } else {
-    return cb(null, false);
+    return callback(null, false);
   }
 }
 
-// Load/create account file
+
 ensureAccount(USERNAME, DOMAIN).then(myaccount => {
   const authWrapper = (req, res, next) => {
     if (req.cookies.token) {
